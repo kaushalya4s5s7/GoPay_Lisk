4244847263b082434cd01bdbdd8ecfd9b56684dc
KC <130903820+kaushalya4s5s7@users.noreply.github.com>
2025-08-04 19:24:21 +0530

    Update hardhat.config.js

--- evm-contracts/hardhat.config.js
+++ evm-contracts/hardhat.config.js
@@ -2,12 +2,9 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
 // Ensure your .env file has the PRIVATE_KEY variable set
-const PRIVATE_KEY = 0x47C20dD1a64F91c0A3590f98266DEABE3536b0A4;
+const PRIVATE_KEY = process.env.PRIVATE_KEY ;
 
-if (!PRIVATE_KEY) {
-  console.error("Please set your PRIVATE_KEY in a .env file");
-  process.exit(1);
-}
+
 
 module.exports = {
   solidity: "0.8.23",
@@ -15,7 +12,7 @@ module.exports = {
     // for testnet
     'lisk-sepolia': {
       url: 'https://rpc.sepolia-api.lisk.com',
-      accounts: [PRIVATE_KEY ],
+      accounts: [process.env.PRIVATE_KEY ],
       gasPrice: 1000000000,
     },
   },
